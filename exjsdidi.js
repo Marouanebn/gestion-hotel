bc1032321fb6c46881791d6c93d0b7a7c41c2500
Your Name <your.email@example.com>
2023-10-31 23:32:52 +0100

    marouane

---
+++ exjsdidi.js
@@ -0,0 +1,104 @@
+// ex 1
+// function sommenmbr(nmr1 , nmr2){
+
+//     console.log(nmr1 + nmr2);
+// }
+
+// sommenmbr(10,10)
+
+// ex 2
+// function minutetosec(minute){
+//     console.log(minute * 60);
+// }
+
+// minutetosec(3);
+// ex 3
+// function nmbrplussone(nmr1){
+    
+//     console.log(nmr1 + 1 )
+
+// }
+// nmbrplussone(4)
+// ex4
+// function trianglesurface(base,hauteur){
+
+//     console.log((base * hauteur)/2);
+
+// }
+// trianglesurface(7,3 );
+// ex5
+// function reversearr(arr="hello"){
+
+//     let x =[];
+     
+//     for (let i = arr.length-1; i >= 0; i--) {
+//         x.push(arr[i]);
+
+        
+        
+//     }
+// return x
+
+
+// }
+// console.log(reversearr());
+
+
+
+
+
+// ex7
+// function getfirst(){
+//     let nmbr1 = [70, 2, 3, 40, 5]
+
+//     let frstnmbr = nmbr1[0];
+
+//     console.log(frstnmbr)
+
+// }
+
+//   getfirst();
+    
+    
+// function restdv(nmbr1,nmbr2){
+
+// let rest = nmbr1 % nmbr2;
+// console.log(rest)
+// } 
+
+// restdv(3,3)
+// serie 2
+// ex2
+// function ex2() {
+//     const prod = [1, 2, 3, 4];
+//     let resultat = 1; // Initialize with 1
+
+//     for (let i = 0; i < prod.length; i++) {
+        
+
+//         if (prod[i] % 2 !== 0) {
+//             resultat *= prod[i]; // Multiply with other odd numbers
+//         }
+//     }
+
+//     console.log(resultat); // Print the final result
+// }
+
+// ex2();
+
+// ex3 
+
+// function ex3(){
+
+// let strg = "bob";
+// let nmr = 2;
+
+// for(let i=0; i<strg.length; i++) {
+
+//     let result = strg[i].repeat(nmr);
+// console.log(result);
+// }
+
+
+// }
+// ex3();
